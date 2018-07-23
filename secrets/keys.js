3f101f2206ec6c1e0ed340e850b30759845d46ea
Gabriel Bolzi <gabriel03899@hotmail.com>
2018-07-22 21:19:39 -0300

    changes

--- secrets/keys.js
+++ secrets/keys.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 if (process.env.NODE_ENV === 'prod') {
   console.log(process.env.NODE_ENV)
+  console.log(1);
   module.exports = require('./dev');
 } else {
+  console.log(process.env.NODE_ENV)
   module.exports = require('./dev');
 }
